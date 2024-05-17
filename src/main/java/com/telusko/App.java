271bc0c15601483a8fc64114ebe18c87b20c7093
Gaurav Sharma <gsharma1154@gmail.com>
2024-05-18 01:23:34 +0530

    get vs load done

--- src/main/java/com/telusko/App.java
+++ src/main/java/com/telusko/App.java
@@ -9,7 +9,7 @@ import org.hibernate.service.ServiceRegistry;
 
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
         // creating session object
 
@@ -23,8 +23,18 @@ public class App {
         Transaction tx = session.beginTransaction();
 
 
+
+        //load gives proxy object.it will not give original object un-till that object is used somewhere in the project.
+        //where as save method gives actual object.save throw a null when the object is not present where as load
+        //throws object not found exception .
+
 //Laptop lap=session.get(Laptop.class,1);
-Laptop lap=session.load(Laptop.class,1);
+Laptop lap=session.load(Laptop.class,101);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         System.out.println(lap);
 
              tx.commit();
